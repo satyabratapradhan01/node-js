1a843ed7be300f9e4bbc23dda983bdb990ebdd35
satyabratapradhan <satyabratapradhann@gmail.com>
2025-06-29 23:58:36 +0530

    req params

--- Express/index.js
+++ Express/index.js
@@ -10,12 +10,17 @@ const app = express();
 const port = process.env.port || 3030;
 
 const static = path.join(__dirname, "public", );
-app.use(express.static(static))
+app.use("/public",express.static(static))
+
+
 // const __dirname = path.dirname(__filename);
 // const homePagePath = path.join(require.main.filename)
+// console.log(import.meta.dirname);
 
-app.get("/", (req, res) => {
-    res.sendFile(index.html);
+
+app.get("/public/:username", (req, res) => {
+    console.log(req.params)
+    res.send(`My userName is ${req.params.username}`)
 });
 
 app.get("/about", (req, res) => {
