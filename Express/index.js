84d1d685c437801628e2457ac4e47c0a88c36e82
satyabratapradhan <satyabratapradhann@gmail.com>
2025-06-29 11:59:39 +0530

    expres

---
+++ Express/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 8080;
+
+app.get("/", (req, res) => {
+    res.send("Helo World!");
+});
+
+app.get("/about", (req, res) => {
+    res.send("Helo About Page!");
+});
+
+app.listen(port, () => {
+    console.log(`App is listening at http://localhost:${port}`);
+});
