f57baa0122f40beb6097779a66896eaa6c52ddd4
satyabratapradhan <satyabratapradhann@gmail.com>
2025-06-30 21:41:01 +0530

    satya

--- Express/index.js
+++ Express/index.js
@@ -13,18 +13,23 @@ const static = path.join(__dirname, "public", );
 app.use("/public",express.static(static))
 
 
-// const __dirname = path.dirname(__filename);
-// const homePagePath = path.join(require.main.filename)
-// console.log(import.meta.dirname);
+const __dirname = path.dirname(__filename);
+const homePagePath = path.join(require.main.filename)
+console.log(import.meta.dirname);
 
 
 app.get("/public/:username", (req, res) => {
-    console.log(req.params)
-    res.send(`My userName is ${req.params.username}`)
+    console.log(req.params);
+    res.send(`My userName is ${req.params.username}`);
 });
 
+app.get("/public/:username/article/:slug", (req, res) => {
+    const formatedSlug = req.params.slug.replace(/-/g, " ")
+    res.send(`Article ${req.params.username} by ${formatedSlug}`)
+})
+
 app.get("/about", (req, res) => {
-   
+    console.log(req.query)
     res.send("Helo About Page!");
 });
 
--- node_satya/math.js
+++
@@ -1,8 +0,0 @@
-// const {add, subs, div, mult} = require('./app.js');
-
-const math = require('../app.js');
-
-console.log(math.add(2, 5));
-console.log(math.mult(2, 5));
-console.log(math.subs(2, 5));
-console.log(math.div(2, 5)); 
