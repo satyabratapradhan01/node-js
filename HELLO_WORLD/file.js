d279ed75c248d70dc3e46bad7fb1ec8adda57555
satyabratapradhan <satyabratapradhann@gmail.com>
2025-07-06 21:47:42 +0530

    working of node js

--- HELLO_WORLD/file.js
+++ HELLO_WORLD/file.js
@@ -19,8 +19,25 @@ const fs = require('fs');
 //     }
 // })
 
-// fs.appendFileSync('./text.txt', 'Hey There\n');
-
+// fs.appendFileSync('./text.txt', 'Hey There\n'); 
 // fs.cpSync('./text.txt', './copy.txt');
-// fs.unlink('./copy.txt', (err) => {})
-fs.mkdirSync('satya.txt');
+// fs.unlink('./copy.txt', (err) => {});
+// fs.mkdirSync('satya.txt');
+// const os = require('os');
+// console.log(os.cpus().length);
+
+
+console.log('1');
+// blocking
+// const result = fs.readFileSync('contacts.txt', 'utf-8');
+// console.log(result);
+
+//non-blocking
+fs.readFile('contacts.txt', 'utf-8', (err, result)=>{
+    console.log(result);
+})
+
+console.log('2');
+console.log('3')
+console.log('4')
+console.log('5')
