9466407be7069fd11762b3ed0b3f54a294431de6
satyabratapradhan <satyabratapradhann@gmail.com>
2025-07-08 18:25:51 +0530

    restfull api

---
+++ project01/index.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const fs = require('fs');
+let users = require('./MOCK_DATA.json');
+const { stringify } = require('querystring');
+
+const app = express();
+const port = 8080;
+
+app.use(express.urlencoded({ extended: false }));
+
+
+app.get('/', (req, res) => {
+    res.send('home page')
+});
+
+app.get("/users", (req, res) => {
+    const html = `
+    <ul>
+    ${users.map((user) => 
+        `<li>${user.first_name}, <br> ${user.last_name}, <br> ${user.email}</li>`
+    )}
+    <ul>`
+    res.send(html)
+})
+
+app.get('/api/users', (req, res) =>{
+    res.json(users);
+});
+
+app.get('/api/users/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find(user => user.id === id);
+    res.json(user)
+})
+
+app.post('/api/users', (req, res) => {
+    const body = req.body;
+    users.push({...body, id: users.length + 1})
+    fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err, data) => {
+        res.json({ status: 'SUCCESS', id: users.length + 1});
+    })
+})
+
+app.patch('/api/users/:id', (req, res) => {
+    res.json({ status: 'Pending'})
+})
+
+app.delete('/api/users/:id', (req, res) => {
+   const id = Number(req.params.id); 
+    users = users.filter(user => user.id !== id);
+    console.log(users);
+   fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err, data) =>{
+    res.json({ status: 'success delete'})
+   })
+})
+
+app.listen(port, ()=>{
+    console.log(`server is listing port no. ${port}`)
+})
